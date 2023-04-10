409695580711404db5f4bc08e77c3a366a6873d0
darkcatneko <darkcat7126@gmail.com>
2023-04-10 19:15:45 +0900

    外側にフォルダを付ける

--- MainGame/Assets/Scripts/BallLauncher.cs
+++ MainGame/Assets/Scripts/BallLauncher.cs
@@ -7,9 +7,8 @@ public class BallLauncher : MonoBehaviour
     [SerializeField] private float launchForce_ = 1f;
     public void BallAddForce()
     {
-        var player = MainGameController.Instance.PlayerObject;
-        var playerRigi = player.GetComponent<Rigidbody>();
-        Vector3 test = new Vector3(0, 0, 1) * launchForce_; 
+        var playerRigi = MainGameController.Instance.PlayerRigibody;
+        Vector3 test = new Vector3(0, 0, 1) * launchForce_;
         playerRigi.AddForce(test);
     }
 }
